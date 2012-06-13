9f5d4792d247bde2654e3d9668455fb60f5dad64
Chris Mytton <self@hecticjeff.net>
2012-06-13 23:50:10 +0100

    Specs for SgNodeMapper.parse_sgn_url

--- spec/sg_node_mapper_spec.rb
+++ spec/sg_node_mapper_spec.rb
@@ -5,4 +5,15 @@ describe SgNodeMapper do
     subject { SgNodeMapper.url_to_graph_node('http://twitter.com/google') }
     it { should == 'sgn://twitter.com/?ident=google' }
   end
+
+  describe ".parse_sgn_url('sgn://twitter.com/?ident=google')" do
+    subject { SgNodeMapper.parse_sgn_url('sgn://twitter.com/?ident=google') }
+
+    it "returns a hash" do
+      subject.should be_a_kind_of(Hash)
+      subject['domain'].should == 'twitter.com'
+      subject['keyName'].should == 'ident'
+      subject['value'].should == 'google'
+    end
+  end
 end
